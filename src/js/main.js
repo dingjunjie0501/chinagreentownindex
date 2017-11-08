116aa690edd99eb9aa0020652215a1e4271be5c2
dingjunjie <dingjunjie@21tb.com>
2017-11-08 11:25:45 +0800

    测试

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ layui.use('element', function () {
         console.log(elem)
         $('.layui-nav-item').removeClass('layui-this');
         $(elem).addClass('layui-this');
+
+        var data = $(elem).children('.header-item-a').attr('data-href');
+        loadNav(data);
     });
 });
 
@@ -63,3 +66,30 @@ layui.use('flow', function () {
         }
     });
 });
+
+function loadNav(data) {
+    layui.use('flow', function () {
+        var flow = layui.flow;
+
+        var elem = '#main-body';
+
+        flow.load({
+            elem: elem, //流加载容器
+            end: ' ',
+            done: function (page, next) { //执行下一页的回调
+                //模拟数据插入
+                setTimeout(function () {
+                    var lis = [];
+                    lis.push('<h2>' + data + '</h2>')
+                    for (var i = 0; i < 8; i++) {
+                        lis.push('<li>' + ((page - 1) * 8 + i + 1) + '</li>')
+                    }
+
+                    //执行下一页渲染，第二参数为：满足“加载更多”的条件，即后面仍有分页
+                    //pages为Ajax返回的总页数，只有当前页小于总页数的情况下，才会继续出现加载更多
+                    next(lis.join(''), page < 10); //假设总页数为 10
+                }, 500);
+            }
+        });
+    });
+}
